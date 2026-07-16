6efef5a5ef796b29a82cf80a2b18d870a37c8784
Thaitheya <thaitheyasudanpk@gmail.com>
2026-07-16 11:31:40 +0530

    Updated Server

--- server/src/main/java/com/sysslan/feedback_management/config/CorsConfig.java
+++ server/src/main/java/com/sysslan/feedback_management/config/CorsConfig.java
@@ -1,32 +1,32 @@
-package com.sysslan.feedback_management.config;
+// package com.sysslan.feedback_management.config;
 
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import java.util.List;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+// import org.springframework.web.cors.CorsConfiguration;
+// import org.springframework.web.cors.CorsConfigurationSource;
+// import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+// import java.util.List;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
 
-@Configuration
-public class CorsConfig {
+// @Configuration
+// public class CorsConfig {
 
-    @Bean CorsConfigurationSource corsConfigurationSource() {
+//     @Bean CorsConfigurationSource corsConfigurationSource() {
 
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("https://amazing-moonbeam-3a3971.netlify.app"));
+//         CorsConfiguration configuration = new CorsConfiguration();
+//         configuration.setAllowedOrigins(List.of("https://amazing-moonbeam-3a3971.netlify.app"));
 
-        configuration.setAllowedMethods(
-                List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+//         configuration.setAllowedMethods(
+//                 List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
 
-        configuration.setAllowedHeaders(List.of("*"));
+//         configuration.setAllowedHeaders(List.of("*"));
 
-        configuration.setAllowCredentials(true);
+//         configuration.setAllowCredentials(true);
 
-        UrlBasedCorsConfigurationSource source =
-                new UrlBasedCorsConfigurationSource();
+//         UrlBasedCorsConfigurationSource source =
+//                 new UrlBasedCorsConfigurationSource();
 
-        source.registerCorsConfiguration("/**", configuration);
+//         source.registerCorsConfiguration("/**", configuration);
 
-        return source;
-    }
-}
+//         return source;
+//     }
+// }
