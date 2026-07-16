da441cda0f9301deaeaccd840edb199c646a72b0
Thaitheya <thaitheyasudanpk@gmail.com>
2026-07-16 16:37:24 +0530

    Updated Server

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -132,7 +132,7 @@ export default function Login() {
                   value={formData.userName}
                   onChange={handleChange}
                   required
-                  className="w-full rounded-lg border border-gray-300 px-3 py-2.5 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                  className="w-full rounded-lg border border-gray-300 px-3 py-2.5 focus:outline-none focus:ring-2 focus:ring-blue-500 text-white"
                 />
               </div>
 
@@ -148,7 +148,7 @@ export default function Login() {
                   value={formData.password}
                   onChange={handleChange}
                   required
-                  className="w-full rounded-lg border border-gray-300 px-3 py-2.5 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                  className="w-full rounded-lg border border-gray-300 px-3 py-2.5 focus:outline-none focus:ring-2 focus:ring-blue-500 text-white"
                 />
               </div>
 
