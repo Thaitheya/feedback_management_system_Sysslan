64f4c1bee5dd08bb38f9094ef684fc8d541c5d13
Thaitheya <thaitheyasudanpk@gmail.com>
2026-07-16 10:12:21 +0530

    Updated Server

--- server/src/main/java/com/sysslan/feedback_management/config/CorsConfig.java
+++ server/src/main/java/com/sysslan/feedback_management/config/CorsConfig.java
@@ -13,8 +13,7 @@ public class CorsConfig {
     @Bean CorsConfigurationSource corsConfigurationSource() {
 
         CorsConfiguration configuration = new CorsConfiguration();
-
-        configuration.setAllowedOrigins(List.of("https://feedbackmanagementsystemts.netlify.app/"));
+        configuration.setAllowedOrigins(List.of("https://amazing-moonbeam-3a3971.netlify.app"));
 
         configuration.setAllowedMethods(
                 List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
