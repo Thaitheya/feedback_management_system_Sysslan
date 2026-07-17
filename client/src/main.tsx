0fb98d0fdc621019fe8227aa79e7c9b890b85805
Thaitheya <thaitheyasudanpk@gmail.com>
2026-07-17 09:03:20 +0530

    Updated Server

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import AuthProvider from "./AuthProvider/AuthProvider";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
-  </React.StrictMode>
+    <BrowserRouter>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </BrowserRouter>
+  </React.StrictMode>,
 );
